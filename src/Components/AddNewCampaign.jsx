8b117451209b67e015e8c53533dd727c4e4a85eb
mazbah2722 <mazbahuddin82269@gmail.com>
2025-03-05 12:38:46 +0600

    AddNewCampaign data get by on submit function

--- src/Components/AddNewCampaign.jsx
+++ src/Components/AddNewCampaign.jsx
@@ -2,32 +2,50 @@ import Navbar from "../Components/Navbar";
 
 
 const AddNewCampaign = () => {
+    const handleAdd=(event)=>{
+        event.preventDefault();
+
+        const form = event.target;
+        const image = form.image.value;
+        const title = form.title.value;
+        const type = form.type.value;
+        const description = form.description.value;
+        const amount = form.amount.value;
+        const deadline = form.deadline.value;
+        const email = form.email.value;
+        const name = form.name.value;
+        const newAddCampaign = {image,title,type,description,amount,deadline,email,name}
+        console.log(newAddCampaign);
+    }
     return (
         <div className="">
             <section>
                     <Navbar></Navbar>
             </section>
-            <h1 className="my-10">Add New Campaign</h1>
-            <form className="flex flex-col justify-center items-center">
-                <div className="grid grid-cols-2 gap-4 w-1/2">
-                    <input type="text" placeholder="Image" className="input input-md" />
-                    <input type="text" placeholder="Campaign title" className="input input-md" />
-                    <select defaultValue="Pick a color" className="select text-gray-500">
+            <h1 className="my-10 text-4xl font-bold">Add New Campaign</h1>
+            <form onSubmit={handleAdd} className="flex flex-col justify-center items-center">
+                <div className=" w-1/2">
+                    <div className="grid grid-cols-2 gap-4">
+                    <input name="image" type="text" placeholder="Image" className="input input-md" />
+                    <input name="title" type="text" placeholder="Campaign title" className="input input-md" />
+                    <select name="type" defaultValue="Type" className="select text-gray-500">
                         <option disabled={true}>Campaign type</option>
                         <option> Personal issue</option>
                         <option>Startup</option>
                         <option>Business</option>
                         <option>Creative ideas</option>
                     </select>
-                    <input type="text" placeholder="Campaign title" className="input input-md" />
-                    <input type="text" placeholder="Description" className="input input-md" />
-                    <input type="number" placeholder="Minimum donation amount" className="input input-md" />
-                    <input type="date" placeholder="Deadline" className="input input-md" />
-                    <input type="text" placeholder="User Email" className="input input-md" />
-                    <input  type="text" placeholder="User Name " className="input input-md" />
-                    <input className="btn" type="submit" value="Add" />
+                    <input name="description" type="text" placeholder="Description" className="input input-md" />
+                    <input name="amount" type="number" placeholder="Minimum donation amount" className="input input-md" />
+                    <input name="deadline" type="date" placeholder="Deadline" className="input input-md" />
+                    <input name="email" type="text" placeholder="User Email" className="input input-md" />
+                    <input name="name"  type="text" placeholder="User Name " className="input input-md" />
+                    
+                    </div>
+                    <input className="btn btn-neutral w-full mt-4" type="submit" value="Add" />
                     
                 </div>
+                
             </form>
         </div>
     );
